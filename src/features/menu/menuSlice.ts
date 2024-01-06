a662b4babfc5078cfeeba972827c0d763009ef76
wayungi <fwayungi.com>
2024-01-06 11:32:06 +0300

    ADD: menuslice

---
+++ src/features/menu/menuSlice.ts
@@ -0,0 +1,72 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import type { RootState } from '../../app/store'
+import { MenuItemType } from '../../custom'
+
+export interface MenuState {
+   menuItems: MenuItemType[]
+}
+
+const initialState: MenuState = {
+  menuItems: [
+    {
+        id: "1",
+        restaurantId: "1",
+        name: "Drum sticks",
+        price: 15000,
+        path: "https://picsum.photos/200",
+    },
+    {
+        id: "2",
+        restaurantId: "1",
+        name: "Pork Ribs",
+        price: 25000,
+        path: "https://picsum.photos/200",
+    },
+    {
+        id: "3",
+        restaurantId: "2",
+        name: "Gum bald",
+        price: 1000,
+        path: "https://picsum.photos/200",
+    },
+    {
+        id: "4",
+        restaurantId: "4",
+        name: "Chips",
+        price: 9000,
+        path: "https://picsum.photos/200",
+    }
+  ]
+}
+
+export const menuSlice = createSlice({
+  name: 'menuItems',
+  initialState,
+  reducers: {
+    getMenuItems: state => {
+      state.menuItems
+    },
+    addMenuItem: (state, action: PayloadAction<MenuItemType>) => {
+      state.menuItems =  [...state.menuItems, action.payload]
+    },
+    updateMenuItems: (state, action:PayloadAction<MenuItemType>) => {
+        state.menuItems = [action.payload, ...state.menuItems.filter((item) => item.id !== action.payload.id)]
+    },
+    deleteMenuItem: (state, action:PayloadAction<string>) => {
+        console.log( action.payload)
+        state.menuItems = state.menuItems.filter((item) => item.id !== action.payload  )
+    }
+  }
+})
+
+export const { 
+    getMenuItems, 
+    addMenuItem, 
+    updateMenuItems,
+    deleteMenuItem,
+ } = menuSlice.actions
+
+export const selectAllRestaurants = (state: RootState) => state.menuItems.menuItems
+export const selectRestaurantById = (state: RootState, id: string) => state.menuItems.menuItems.filter((item) =>  item.id === id)[0]
+
+export default menuSlice.reducer
