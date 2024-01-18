f4eee81cc65ba0617eb2441679a10d9360e43147
wayungi <fwayungi.com>
2024-01-18 14:39:43 +0300

    ADD: basic react router lay out

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
-import RestaurantList from "./components/RestaurantList"
-import NewRestaurant from "./components/NewRestaurant"
-import UpdateRestaurant from "./components/UpdateRestaurant"
-import GeneralMenu from "./components/GeneralMenu"
-import RestaurantMenu from "./components/RestaurantMenu"
+// import RestaurantList from "./components/RestaurantList"
+// import NewRestaurant from "./components/NewRestaurant"
+// import UpdateRestaurant from "./components/UpdateRestaurant"
+// import GeneralMenu from "./components/GeneralMenu"
+// import RestaurantMenu from "./components/RestaurantMenu"
 
 function App() {
   return (
     <main className="p-1 h-screen">
-      {/* <RestaurantList /> */}
-      {/* <NewRestaurant /> */}
-      {/* <UpdateRestaurant id="1"/> */}
-      {/* <GeneralMenu /> */}
-      <RestaurantMenu id="1" />
+      <div>
+        <p>Side bar content</p>
+      </div>
+      {/* <RestaurantList />
+      <NewRestaurant />
+      <UpdateRestaurant id="1"/>
+      <GeneralMenu />
+      <RestaurantMenu id="1" /> */}
     </main> 
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -5,10 +5,52 @@ import './index.css'
 import store from './app/store'
 import { Provider } from 'react-redux'
 
+import RestaurantList from "./components/RestaurantList"
+import NewRestaurant from "./components/NewRestaurant"
+import UpdateRestaurant from "./components/UpdateRestaurant"
+import GeneralMenu from "./components/GeneralMenu"
+import RestaurantMenu from "./components/RestaurantMenu"
+
+// import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  // Link,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/", /*root route*/
+    element: <App />,
+  },
+  {
+    path: "/restaurantList",
+    element: <RestaurantList />,
+  },
+  {
+    path: "newRestaurant",
+    element: <NewRestaurant />,
+  },
+  {
+    path: "UpdateRestaurant",
+    element: <UpdateRestaurant id='1'/>,
+  },
+  {
+    path: "GeneralMenu",
+    element: <GeneralMenu />,
+  },
+  {
+    path: "RestaurantMenu",
+    element: <RestaurantMenu  id="1"/>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+        {/* <App /> */}
+      </Provider>
   </React.StrictMode>,
 )
