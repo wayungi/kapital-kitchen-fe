38913787cc38666efe1d35b6f5bdc9dc03b9763a
wayungi <fwayungi.com>
2024-01-26 11:50:34 +0300

    ADD: display cartList items

--- src/components/cart/Cart.tsx
+++ src/components/cart/Cart.tsx
@@ -1,8 +1,19 @@
+// import { useState } from 'react'
+// import { CartType } from '../../custom'
+import { useAppSelector } from '../../app/hooks'
+import { selectCartItems } from '../../features/cart/cartSlice'
+import CartDisplayLine from './CartDisplayLine'
+
 const Cart = () => {
+    const cartList = useAppSelector((state) => selectCartItems(state))
+    //const [cartItems, setCartItems] =  useState<CartType[] | undefined>(cartList) 
+
+    const cart = cartList.map((orderObj) => <CartDisplayLine orderObject={orderObj} />)
+      
 
     return (
         <section>
-            <h1>Cart</h1>
+            <h1>{cart}</h1>
         </section>
     )
     
