4ccd2be4d7e5714ae3a9c48509011ff25a0ab97f
wayungi <fwayungi.com>
2024-01-26 15:08:20 +0300

    ADD: validation against duplicate entries in cart & removed commented code

--- src/features/cart/cartSlice.ts
+++ src/features/cart/cartSlice.ts
@@ -2,92 +2,11 @@ import { /*createAsyncThunk,*/ createSlice, /*PayloadAction*/ } from "@reduxjs/t
 import type { RootState } from "../../app/store";
 import { CartType } from "../../custom";
 
-// const BASE_URL = "http://127.0.0.1:3000";
-// const postHeader = {
-//   method: "POST",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// };
-
-// const getHeader = {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// };
-
-// const deleteHeader = {
-//   method: "DELETE",
-//   header: {
-//     "Content-Type": "application/json",  }
-// }
-
 export interface CartState {
   orderList: CartType[];
   loading: "idle" | "pending" | "completed" | "failed";
   error: undefined| string/*undefined was initially null, change it to resolve error in the extra reduces rejected case*/;
 }
-
-
-// export const fetchMenu = createAsyncThunk("menu/fetchmenu", async () => {
-//   try {
-//     const response = await fetch(`${BASE_URL}/menu`, {
-//       ...getHeader,
-//     });
-//     if (!response.ok) return;
-//     const result = await response.json();
-//     return result
-//   } catch (err) {
-//     console.log(err);
-//   }
-// });
-
-// export const saveMenu = createAsyncThunk(
-//   "menu/saveMenu",
-//   async (menuItem: MenuItemType) => {
-//     try {
-//       const response = await fetch(`${BASE_URL}/menu`, {
-//         ...postHeader,
-//         body: JSON.stringify(menuItem),
-//       });
-//       const result = await response.json();
-//       return result;
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   }
-// );
-
-// export const deleteMenuItem = createAsyncThunk("menu/deleteMenuItem", async(id: string) => {
-//   try {
-//     const response = await fetch(`${BASE_URL}/menu/${id}`, {
-//       ...deleteHeader,
-//     })
-//     if(!response) return 
-//     const result =  await response.json()
-//     return result.response
-//   }catch(err) {
-//     console.log(err)
-//   }
-// })
-
-// export const updateMenuItem = createAsyncThunk('menu/updateMenuItem', async(menuItem: MenuItemType) => {
-//   try {
-//   const response = await fetch(`${BASE_URL}/menu/${menuItem._id}`, {
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json"
-//     },
-//     body: JSON.stringify(menuItem)
-//   })
-//   if(!response) return
-//   const result =  await response.json()
-//   return result.response
-//   }catch(err) {
-//     console.log(err)
-//   }
-// })
 
 const initialState: CartState = {
   orderList: [],
@@ -95,12 +14,14 @@ const initialState: CartState = {
   error: ""
 }
 
-
 export const cartSlice = createSlice({
   name: "carItems",
   initialState,
   reducers: {
     addItemToCart: (state, action) => {
+      const id =  action.payload.menuId
+      const exists = state.orderList.find((item) => item.menuId === id)
+      if(exists) return
       state.orderList = [...state.orderList, action.payload]
     },
     updateQuantity: (state, action) => {
@@ -118,24 +39,7 @@ export const cartSlice = createSlice({
     },
   },
   extraReducers: (builder) => 
-  builder
-//   .addCase(fetchMenu.pending, (state) => {
-//     state.loading = "pending"
-//   })
-//   .addCase(fetchMenu.fulfilled, (state, action) => {
-//     state.loading = "completed"
-//     state.menuItems = [...action.payload]
-//   })
-//   .addCase(fetchMenu.rejected, (state, action) => {
-//     state.error = action.error.message
-//     state.loading = "failed"
-//   })
-//   .addCase(deleteMenuItem.fulfilled, (state, action) => {
-//     console.log(action.payload)
-//     state.menuItems =  state.menuItems.filter((item) => item._id !== action.payload._id)
-//   })
-
-  
+  builder  
 });
 
 export const { addItemToCart,  updateQuantity, removeItemFromCart } = cartSlice.actions;
