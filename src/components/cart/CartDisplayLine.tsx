8703fba8aedf291429c9310d8c69c8d22dabb96c
wayungi <fwayungi.com>
2024-01-26 14:11:07 +0300

    ADD: state update on quantity change

--- src/components/cart/CartDisplayLine.tsx
+++ src/components/cart/CartDisplayLine.tsx
@@ -1,28 +1,30 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { CartType } from "../../custom"
+import { updateQuantity } from '../../features/cart/cartSlice'
+import { useAppDispatch } from '../../app/hooks'
 
 type cartDisplayLineProp = {
     orderObject: CartType
 }
 const CartDisplayLine = ({ orderObject }:  cartDisplayLineProp ) => {
+    const dispatch = useAppDispatch()
 
-    const { restaurantId, menuId, name, quantity, price, restaurant, path } = orderObject
+    const { menuId, name, quantity, price, restaurant, path } = orderObject
     const [alteredQuantity, setAlteredQuantity] = useState<number>(quantity)
 
-    const decreaseQuantity = () => {
-        if(alteredQuantity <= 0) return
-        setAlteredQuantity((prev:number) => prev -1)
-    }
-
-    const increaseQunatity = () => {
-        setAlteredQuantity( (prev: number) => prev + 1)
-    }
+    useEffect(()  => {
+        setAlteredQuantity(alteredQuantity)
+        dispatch(updateQuantity({ alteredQuantity, menuId }))
+    }, [alteredQuantity, dispatch, menuId])
 
     return (
         <>
             <div>{path}</div>
             <div>{name}</div>
-            <div> <button onClick={ decreaseQuantity }>-</button> {alteredQuantity} <button onClick={ increaseQunatity }>+</button></div>
+            <div> 
+                <button onClick={ () => setAlteredQuantity((prev) => prev === 1 ? 1 : prev - 1) }>-</button> 
+                    {alteredQuantity} 
+                <button onClick={ () => setAlteredQuantity((prev) => prev + 1)}>+</button></div>
             <div>{price}</div>
             <div>{quantity * price}</div>
             <div>{restaurant}</div>
